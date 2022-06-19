a492824b546f98e9a506543ff033ab25adbc38d1
Vlad <vlad.markevichh@gmail.com>
2022-06-19 15:12:32 +0300

    add Outline

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import s from './App.module.scss';
-import Content from './Components/Content/Content';
-import TopMenu from './Components/TopMenu/TopMenu';
+import Friends from './Components/Content/Friends/Friends';
+import Home from './Components/Content/Home/Home';
+import Test from './Components/Content/Test/Test';
+import Layout from './Components/Layout/Layout';
+import { Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <div className={s.bodyApp}>
-
-      <TopMenu/>
-      <Content/>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='friends' element={<Friends />} />
+          <Route path='test' element={<Test />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import s from './Layout.module.scss';
+import { Link, Outlet } from 'react-router-dom';
+
+const Layout = () => {
+    return (
+        <div className={s.bodyLayout}>
+        
+            <header className={s.bodyTopMenu}>
+                <Link to='/'>Home</Link>
+                <Link to='/friends'>Friends</Link>
+                <Link to='/test'>Test</Link>
+            </header>
+            <main className={s.container}>
+                <Outlet/>
+            </main>
+            
+            <footer>
+                <div>
+                  2022  
+                </div>
+                
+            </footer>
+ 
+        </div>
+        
+    )
+}
+
+export default Layout;
